8f77d1a8b24670780fb3968921b24be36602d276
Espérance Engineer <alladoumndoubaesperance@gmail>
2022-11-09 22:04:11 +0100

    Model User modifé

--- users/admin.py
+++ users/admin.py
@@ -50,7 +50,7 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_admin', )
     fieldsets = (
         (None, {'fields': ('email','password',)}),
-        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Personal info', {'fields': ('experience',)}),
         ('Permissions', {'fields': ('is_admin','is_active')}),
         ('Site Info', {'fields': ('credits', 'status','situation')}),
     )
